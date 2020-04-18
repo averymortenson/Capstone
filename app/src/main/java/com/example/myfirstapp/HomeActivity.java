b9f276dfc54be2571edc2cce38a8d791a3f3cc8b
Avery Mortenson <49291961+averymortenson@users.noreply.github.com>
2020-04-17 19:34:36 -0700

    4/17/2020 7:33pm Bottom Nav Bar Implementation
    Nav bar up but not yet fully functional.

--- app/src/main/java/com/example/myfirstapp/HomeActivity.java
+++ app/src/main/java/com/example/myfirstapp/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
@@ -20,12 +23,33 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         btnLogout = findViewById(R.id.logout);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottonNavBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.searchIcon:
+                        break;
+                    case R.id.homeIcon:
+
+                        break;
+                    case R.id.messageIcon:
+
+                        break;
+                    case R.id.userIcon:
+
+                        break;
+                }
+                return false;
+            }
+        });
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
-                Intent intToMain = new Intent(HomeActivity.this,MainActivity.class);
-                startActivity(intToMain);
+                Intent intToLogin = new Intent(HomeActivity.this,LoginActivity.class);
+                startActivity(intToLogin);
             }
         });
     }
